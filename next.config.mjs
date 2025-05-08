7544b71bf8c0c88306a580a1f2ee7d1dc4e8bff0
暗蓝 Alan Hu <alanhu1024@163.com>
2025-05-08 20:52:02 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    NODE_ENV: process.env.NODE_ENV,
-  },
   experimental: {
     instrumentationHook: false
   }
